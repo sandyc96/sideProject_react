cb549f4414250087af2485993f215b8a39918caa
yc <star851215@gmail.com>
2026-05-07 18:41:14 +0800

    Reapply "add google login"
    This reverts commit f59acc4a4b708447baf8866739696d2ae5a8407c.

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { createTheme, ThemeProvider } from '@mui/material';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const theme = createTheme({
   palette: {
@@ -40,9 +41,11 @@ const theme = createTheme({
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  </GoogleOAuthProvider>,
 );
